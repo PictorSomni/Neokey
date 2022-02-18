5d09d5fa8fca465d9b3eb57cafa79a013e5d014d
Charles Dessenius <dessenius.charles@gmail.com>
2022-02-18 10:25:06 +0100

    2 usages of the NeoKey Trinkey
    ITERATE --> maintain touch than click once the rename the selected file of the explorer / finder to increment it's name (useful when you want your files to be seen in a particuliar order)
    
    Click twice and the added increment will be removed (attention, the device will juste erase the 3 first characters of the selected file, pay attention to what is currently selected)...
    
    Click 4 times and the current window will close (ALT+F4)
    
    COUNTER -->  maintain touch than click any times you want to increase the counter. When you'll let go of the touchpad, the selected fil on you explorer / finder will be renamed to start with "(the number of times you clicked)X_".
    
    Usefull when you want to print x times a particuliar photo.
    
    If you click the button without maintaining the touchpad,  the device will juste erase the 3 first characters of the selected file, pay attention to what is currently selected)...

---
+++ COUNTER.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+#############################################################
+#                          IMPORTS                          #
+#############################################################
+import time
+import board
+import neopixel
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode  # pylint: disable=unused-import
+from digitalio import DigitalInOut, Pull
+import touchio
+
+#############################################################
+#                          CONTENT                          #
+#############################################################
+print("NeoKey Trinkey HID")
+counter = 0
+
+## NEOPIXEL
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
+pixel.fill((255, 0, 255))
+
+## KEYBOARD HID
+time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
+
+## BUTTON
+# create the switch, add a pullup, start it with not being pressed
+button = DigitalInOut(board.SWITCH)
+button.switch_to_input(pull=Pull.DOWN)
+button_state = False
+
+## TOUCH
+# create the captouch element and start it with not touched
+touch = touchio.TouchIn(board.TOUCH)
+touch_state = False
+
+## THE REMOVE SEQUENCE
+remove = (
+   {'keys': Keycode.F2, 'delay': 0.2},
+   {'keys': Keycode.HOME, 'delay': 0.1},
+   {'keys': Keycode.DELETE, 'delay': 0.1},
+   {'keys': Keycode.DELETE, 'delay': 0.1},
+   {'keys': Keycode.DELETE, 'delay': 0.1},
+   {'keys': Keycode.ENTER, 'delay': 0.1}
+)
+
+#############################################################
+#                         FUNCTION                          #
+#############################################################
+def make_keystrokes(keys, delay):
+    if isinstance(keys, str):  # If it's a string...
+        keyboard_layout.write(keys)  # ...Print the string
+    elif isinstance(keys, int):  # If its a single key
+        keyboard.press(keys)  # "Press"...
+        keyboard.release_all()  # ..."Release"!
+    elif isinstance(keys, (list, tuple)):  # If its multiple keys
+        keyboard.press(*keys)  # "Press"...
+        keyboard.release_all()  # ..."Release"!
+    time.sleep(delay)
+
+#############################################################
+#                         MAIN LOOP                         #
+#############################################################
+while True:
+    if touch.value and not touch_state: 
+        pixel.fill((0, 128, 255))
+        touch_state = True
+        print("touch")
+        while touch.value :
+            if button.value and not button_state:
+                button_state = True
+
+            if not button.value and button_state:
+                counter += 1
+                button_state = False
+
+    sequence = ( ## Need to put this here otherwise index isn't updated
+        {'keys': Keycode.F2, 'delay': 0.2},
+        {'keys': Keycode.HOME, 'delay': 0.1},
+        # {'keys': "{:02}X_\n".format(counter), 'delay': 0.1}
+        {'keys': "{}X_\n".format(counter), 'delay': 0.1}
+    )
+
+    if not touch.value and touch_state:
+        print("no touch")
+        print(counter)
+        touch_state = False
+        for k in sequence:
+            make_keystrokes(k['keys'], k['delay'])
+        counter = 0
+        pixel.fill((255, 0, 255))
+
+    if button.value and not button_state:
+        button_state = True
+
+    if not button.value and button_state:
+        for k in remove:
+            make_keystrokes(k['keys'], k['delay'])
+        button_state = False
